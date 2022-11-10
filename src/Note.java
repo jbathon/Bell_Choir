c8da6d5da6444d6c2f7a572de9955d8913846dca
Jaden Bathon <jbathon@carroll.edu>
2022-11-10 10:40:12 -0700

    Removed the mythical E Sharp

--- src/Note.java
+++ src/Note.java
@@ -7,10 +7,10 @@
 
 public enum Note {
   // REST Must be the first 'Note'
-  REST, A0, A0S, B0, C0, C0S, D0, D0S, E0, E0S, F0, F0S, G0, G0S, A1, A1S, B1, C1, C1S, D1, D1S, E1, E1S, F1, F1S, G1,
-  G1S, A2, A2S, B2, C2, C2S, D2, D2S, E2, E2S, F2, F2S, G2, G2S, A3, A3S, B3, C3, C3S, D3, D3S, E3, E3S, F3, F3S, G3,
-  G3S, A4, A4S, B4, C4, C4S, D4, D4S, E4, E4S, F4, F4S, G4, G4S, A5, A5S, B5, C5, C5S, D5, D5S, E5, E5S, F5, F5S, G5,
-  G5S, A6, A6S, B6, C6, C6S, D6, D6S, E6, E6S, F6, F6S, G6, G6S, A7, A7S, B7, C7, C7S, D7, D7S, E7, E7S, F7, F7S, G7,
+  REST, A0, A0S, B0, C0, C0S, D0, D0S, E0, F0, F0S, G0, G0S, A1, A1S, B1, C1, C1S, D1, D1S, E1, F1, F1S, G1,
+  G1S, A2, A2S, B2, C2, C2S, D2, D2S, E2, F2, F2S, G2, G2S, A3, A3S, B3, C3, C3S, D3, D3S, E3, F3, F3S, G3,
+  G3S, A4, A4S, B4, C4, C4S, D4, D4S, E4, F4, F4S, G4, G4S, A5, A5S, B5, C5, C5S, D5, D5S, E5, F5, F5S, G5,
+  G5S, A6, A6S, B6, C6, C6S, D6, D6S, E6, F6, F6S, G6, G6S, A7, A7S, B7, C7, C7S, D7, D7S, E7, F7, F7S, G7,
   G7S, A8, A8S, B8, C8;
 
   public static final int SAMPLE_RATE = 192 * 1024; // ~192KHz so the sound is smoother.
@@ -54,6 +54,7 @@ public enum Note {
    */
 
   public static Note getNote(String str) throws InvalidNoteException {
+    if()
     try {
       return Note.valueOf(str);
     } catch (Exception e) {
